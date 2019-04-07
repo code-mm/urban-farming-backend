34f2b187d1f185b78892f84bd9a3ef466c63f485
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-07 19:34:07 +0200

    Adds example data for database

---
+++ database_example_data/device.sql
@@ -0,0 +1,2 @@
+INSERT INTO device (name, identifier, secret, model_workspace_id) VALUES ('device1', 'd34be04a-55fc-11e9-aca6-e86a642d909e', 'testsecret', 1);
+INSERT INTO device (name, identifier, secret, model_workspace_id) VALUES ('device1', 'd34be04a-55fc-11e9-aca6-e86a642d9091', 'testsecret', 1);
---
+++ database_example_data/user.sql
@@ -0,0 +1,1 @@
+INSERT INTO "user" (firstname, lastname, email) VALUES ('John', 'Doe', 'john.doe@example.com');
