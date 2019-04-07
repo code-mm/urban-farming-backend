503ccd084dc26958108cc10f02ece4520c45fe9b
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-07 19:40:45 +0200

    Adds device endpoint with router and authentication middleware

--- router.go
+++ router.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func BaseRouter() *mux.Router {
 	router := mux.NewRouter()
 	return router
+}
+
+func DeviceRouter(router *mux.Router) {
+	deviceSubrouter := router.PathPrefix("/device").Subrouter()
+    deviceSubrouter.Handle("/", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(Device))).Methods("GET")
+
+    deviceSubrouter.Handle("/datapoint", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPoint))).Methods("GET")
+    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointPh))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint/oxygen", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointOxygen))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint/temperature", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointTemperature))).Methods("GET", "POST")
+
+    deviceSubrouter.HandleFunc("/authentication/gettoken", DeviceAuthenticationGetToken).Methods("POST")
 }
