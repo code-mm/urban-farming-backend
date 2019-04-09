5fee95d4916af8b96a4491ac9f35a8dbc6a0ce7b
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-09 21:50:13 +0200

    Moves device authentication to its own endpoint

---
+++ authentication.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "net/http"
+    "time"
+    "io"
+    "github.com/SermoDigital/jose/jws"
+    "github.com/SermoDigital/jose/crypto"
+)
+
+func AuthenticationGetTokenDevice(w http.ResponseWriter, r *http.Request) {
+    // check if we got application/x-www-form-urlencoded content type
+    if r.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
+        w.WriteHeader(http.StatusUnsupportedMediaType)
+        return
+    }
+
+    // extract device identifier and secret
+    deviceIdentifier := r.FormValue("deviceIdentifier")
+    deviceSecret := r.FormValue("deviceSecret")
+
+    // check if identifier and secret are valid
+    device := new(ModelDevice)
+    exists, err := Db.Model(device).Where("Identifier = ?", deviceIdentifier).Where("Secret = ?", deviceSecret).Exists()
+
+    if err != nil {
+        panic(err)
+    }
+
+    if !exists {
+        w.WriteHeader(http.StatusUnauthorized)
+        return
+    }
+
+    // generate json web token
+    claims := jws.Claims{}
+    claims.SetSubject(deviceIdentifier)
+    claims.SetIssuedAt(time.Now())
+    claims.SetExpiration(time.Now().Add(time.Duration(3600) * time.Second))
+    token, _ := jws.NewJWT(claims, crypto.SigningMethodHS256).Serialize([]byte("your-256-bit-secret"))
+
+    // return the web token
+    w.Header().Set("Content-Type", "application/jwt")
+    io.WriteString(w, string(token[:]))
+}
