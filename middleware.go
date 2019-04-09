c236d21c861808ce88ba8f31b04c2b2bfe227662
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-09 22:54:13 +0200

    Simplifies jwt validation middleware and renames function according to common name scheme

--- middleware.go
+++ middleware.go
@@ -2,43 +2,27 @@ package main
 
 import (
 	"net/http"
-	"regexp"
-	"strings"
 	"github.com/gorilla/context"
 	"github.com/SermoDigital/jose/jws"
     "github.com/SermoDigital/jose/crypto"
 )
 
-func DeviceJwtTokenValidation(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	// check if authorization header in place and get it if so
-	authorizationHeader := r.Header.Get("Authorization")
-
-	// check format of authorization header
-	authorizationHeaderRegex, _ := regexp.Compile("^Bearer [A-Za-z0-9.]+")
-
-	if !authorizationHeaderRegex.MatchString(authorizationHeader) {
-		rw.WriteHeader(http.StatusUnauthorized)
-        return
-	}
-
-	// extract access token from authorization header
-	accessToken := strings.TrimPrefix(authorizationHeader, "Bearer ")
-
-	// check if jwt is valid
-	jwt, err := jws.ParseJWT([]byte (accessToken))
+func JwtTokenValidationDevice(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// Parse jwt from request header authorization field
+	token, err := jws.ParseJWTFromRequest(r)
 
 	if err != nil {
 		rw.WriteHeader(http.StatusUnauthorized)
         return
 	}
 
-	if err = jwt.Validate([]byte("your-256-bit-secret"), crypto.SigningMethodHS256); err != nil {
+	if err = token.Validate([]byte("your-256-bit-secret"), crypto.SigningMethodHS256); err != nil {
 		rw.WriteHeader(http.StatusUnauthorized)
         return
 	}
 
 	// get the deviceIdentifier from json jwt and store it in the request context
-	deviceIdentifier := jwt.Claims()["sub"]
+	deviceIdentifier := token.Claims()["sub"]
 	context.Set(r, "deviceIdentifier", deviceIdentifier)
 
     next(rw, r)
--- router.go
+++ router.go
@@ -17,10 +17,10 @@ func AuthenticationRouter(router *mux.Router) {
 
 func DeviceRouter(router *mux.Router) {
 	deviceSubrouter := router.PathPrefix("/device").Subrouter()
-    deviceSubrouter.Handle("/", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(Device))).Methods("GET")
+    deviceSubrouter.Handle("/", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(Device))).Methods("GET")
 
-    deviceSubrouter.Handle("/datapoint", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPoint))).Methods("GET")
-    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointPh))).Methods("GET", "POST")
-    deviceSubrouter.Handle("/datapoint/oxygen", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointOxygen))).Methods("GET", "POST")
-    deviceSubrouter.Handle("/datapoint/temperature", negroni.New(negroni.HandlerFunc(DeviceJwtTokenValidation), negroni.WrapFunc(DeviceDataPointTemperature))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPoint))).Methods("GET")
+    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointPh))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint/oxygen", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointOxygen))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint/temperature", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointTemperature))).Methods("GET", "POST")
 }
