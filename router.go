d3e01ff1622ade1caf0e0950d78a59d01ad2066a
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-18 14:39:15 +0200

    Splits device datapoint ph into two functions to seperate get and post requests. Also implements their functionality.

--- router.go
+++ router.go
@@ -18,7 +18,8 @@ func AuthenticationRouter(router *mux.Router) {
 func DeviceRouter(router *mux.Router) {
 	deviceSubrouter := router.PathPrefix("/device").Subrouter()
     deviceSubrouter.Handle("/", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(Device))).Methods("GET")
-    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointPh))).Methods("GET", "POST")
+    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointPhList))).Methods("GET")
+    deviceSubrouter.Handle("/datapoint/ph", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointPhCreate))).Methods("POST")
     deviceSubrouter.Handle("/datapoint/oxygen", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointOxygen))).Methods("GET", "POST")
     deviceSubrouter.Handle("/datapoint/temperature", negroni.New(negroni.HandlerFunc(JwtTokenValidationDevice), negroni.WrapFunc(DeviceDataPointTemperature))).Methods("GET", "POST")
 }
