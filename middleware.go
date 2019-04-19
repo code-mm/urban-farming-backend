2c0c0e9c137eb872610f7ab82538c950ee07608e
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-19 20:04:58 +0200

    Adds middleware for json content type validation on post requestes

--- middleware.go
+++ middleware.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
+	"bytes"
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/context"
 	"github.com/SermoDigital/jose/jws"
     "github.com/SermoDigital/jose/crypto"
@@ -26,4 +29,27 @@ func JwtTokenValidationDevice(rw http.ResponseWriter, r *http.Request, next http
 	context.Set(r, "deviceIdentifier", deviceIdentifier)
 
     next(rw, r)
+}
+
+func ContentTypeValidationJson(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	// check content type in request header
+	if r.Header.Get("Content-Type") != "application/json" {
+		rw.WriteHeader(http.StatusUnsupportedMediaType)
+		return
+	}
+
+	// get body from request
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	// check if json in body is valid
+	if !json.Valid([]byte(requestBody)) {
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// reset the request body
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(requestBody))
+
+	next(rw, r)
 }
