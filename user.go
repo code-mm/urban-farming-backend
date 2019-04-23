0ed45019ba872d0327450fea786f0af11826bc32
Markus Merklinger <markus.merklinger@code.berlin>
2019-04-23 15:37:45 +0200

    Adds datapoint query for user endpoint

--- user.go
+++ user.go
@@ -3,6 +3,7 @@ package main
 import (
     "net/http"
     "encoding/json"
+    "github.com/gorilla/mux"
     "github.com/gorilla/context"
 )
 
@@ -27,6 +28,7 @@ func User(w http.ResponseWriter, r *http.Request) {
     w.Write(result)
 }
 
+
 /*
  * user device list
  */
@@ -46,3 +48,142 @@ func User(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     w.Write(result)
  }
+
+
+/*
+ * user device datapoint
+ */
+func UserDeviceDatapointPh(w http.ResponseWriter, r *http.Request) {
+    // check if device exists
+    params := mux.Vars(r)
+
+    var device []ModelDevice
+    exists, err := Db.Model(&device).Where("id = ?", params["deviceId"]).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // check if user has read access to device
+    var deviceAccess ModelUserDeviceAccess
+    exists, err = Db.Model(&deviceAccess).Join("JOIN \"user\" ON model_user_device_access.model_user_id = \"user\".id").Where("model_user_device_access.model_device_id=?", params["deviceId"]).Where("model_user_device_access.read=?", true).Where("\"user\".email=?", context.Get(r, "email").(string)).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // fetch data from database
+    var dataPoint []ModelDeviceDataPointPh
+    if _, err := Db.Query(&dataPoint, `SELECT device_datapoint_ph.time, device_datapoint_ph.value FROM device_datapoint_ph WHERE model_device_id = ?`, params["deviceId"]); err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    // marshal data to json
+    result, err := json.Marshal(dataPoint)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(result)
+}
+
+func UserDeviceDatapointOxygen(w http.ResponseWriter, r *http.Request) {
+    // check if device exists
+    params := mux.Vars(r)
+
+    var device []ModelDevice
+    exists, err := Db.Model(&device).Where("id = ?", params["deviceId"]).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // check if user has read access to device
+    var deviceAccess ModelUserDeviceAccess
+    exists, err = Db.Model(&deviceAccess).Join("JOIN \"user\" ON model_user_device_access.model_user_id = \"user\".id").Where("model_user_device_access.model_device_id=?", params["deviceId"]).Where("model_user_device_access.read=?", true).Where("\"user\".email=?", context.Get(r, "email").(string)).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // fetch data from database
+    var dataPoint []ModelDeviceDataPointPh
+    if _, err := Db.Query(&dataPoint, `SELECT device_datapoint_oxygen.time, device_datapoint_oxygen.value FROM device_datapoint_oxygen WHERE model_device_id = ?`, params["deviceId"]); err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    // marshal data to json
+    result, err := json.Marshal(dataPoint)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(result)
+}
+
+func UserDeviceDatapointTemperature(w http.ResponseWriter, r *http.Request) {
+    // check if device exists
+    params := mux.Vars(r)
+
+    var device []ModelDevice
+    exists, err := Db.Model(&device).Where("id = ?", params["deviceId"]).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // check if user has read access to device
+    var deviceAccess ModelUserDeviceAccess
+    exists, err = Db.Model(&deviceAccess).Join("JOIN \"user\" ON model_user_device_access.model_user_id = \"user\".id").Where("model_user_device_access.model_device_id=?", params["deviceId"]).Where("model_user_device_access.read=?", true).Where("\"user\".email=?", context.Get(r, "email").(string)).Exists()
+    if err != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    if exists == false {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    // fetch data from database
+    var dataPoint []ModelDeviceDataPointPh
+    if _, err := Db.Query(&dataPoint, `SELECT device_datapoint_temperature.time, device_datapoint_temperature.value FROM device_datapoint_temperature WHERE model_device_id = ?`, params["deviceId"]); err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    // marshal data to json
+    result, err := json.Marshal(dataPoint)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(result)
+}
